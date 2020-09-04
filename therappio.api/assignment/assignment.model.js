0555b13c931b721aee3ff633c50c9e5920c40ea0
Natalia <mcb.nat@gmail.com>
2020-09-05 00:44:10 +0200

    Update client id generation

--- therappio.api/assignment/assignment.model.js
+++ therappio.api/assignment/assignment.model.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const nanoid = require("nanoid");
-const { Schema } = mongoose;
+const {
+  Schema
+} = mongoose;
 
 const questionTypes = [
   "short",
@@ -18,19 +20,45 @@ const questionTypes = [
 const status = ["Not submitted", "Late", "On time"];
 
 const TaskSchema = new Schema({
-  question: { type: String, required: true },
-  type: { type: String, enum: questionTypes, required: true },
+  question: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    enum: questionTypes,
+    required: true
+  },
   answer: [String],
   options: [String]
 });
 
 const AssignmentSchema = new Schema({
-  _id: { type: String, default: () => nanoid() },
-  client: { type: String, ref: "User", required: true },
-  createdAt: { type: Date, required: true, default: Date.now },
-  dueDate: { type: Date, required: true },
+  _id: {
+    type: String,
+    default: () => nanoid()
+  },
+  client: {
+    type: String,
+    ref: "User",
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  dueDate: {
+    type: Date,
+    required: true
+  },
   submittedAt: Date,
-  status: { type: String, required: true, default: status[0], enum: status },
+  status: {
+    type: String,
+    required: true,
+    default: status[0],
+    enum: status
+  },
   fields: [TaskSchema],
   title: String
 });
