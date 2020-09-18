47aa84b247919d246f9c497581fda1880deee29c
Natalia <mcb.nat@gmail.com>
2020-09-19 00:21:29 +0200

    Update querying ICD-10 dictionary

--- therappio.api/icd10-code/icd10Code.controller.js
+++ therappio.api/icd10-code/icd10Code.controller.js
@@ -2,28 +2,14 @@ const express = require('express');
 const router = express.Router();
 const icd10CodeService = require('./icd10Code.service');
 const authorize = require('_helpers/authorize');
-const Role = require('_helpers/role');
 
 // routes
-router.get('/', authorize(), get);
+router.get('/', authorize(), function get(req, res, next) {
+  icd10CodeService
+    .query(req.query.q || '')
+    .then(icd10Codes => {
+      return res.json(icd10Codes);
+    })
+    .catch(err => next(err));
+});
 module.exports = router;
-
-function get(req, res, next) {
-  if (!req.query.code && !req.query.id)
-    icd10CodeService
-      .getAll()
-      .then(icd10Codes => res.json(icd10Codes))
-      .catch(err => next(err));
-
-  if (req.query.id)
-    icd10CodeService
-      .getById(req.query.id)
-      .then(icd10Codes => res.json(icd10Codes))
-      .catch(err => next(err));
-
-  if (req.query.code)
-    icd10CodeService
-      .getByFullCode(req.query.code)
-      .then(icd10Codes => res.json(icd10Codes))
-      .catch(err => next(err));
-}
--- therappio.api/icd10-code/icd10Code.service.js
+++ therappio.api/icd10-code/icd10Code.service.js
@@ -2,19 +2,24 @@ const db = require('_helpers/db');
 const Icd10Code = db.Icd10Code;
 
 module.exports = {
-  getAll,
-  getById,
-  getByFullCode,
+  query: async function query(text) {
+    if (!text) return await Icd10Code.find().select('-__v');
+    console.log(text);
+    return await Icd10Code.find({
+      $or: [
+        {
+          fullCode: new RegExp('.*' + text + '.*', 'i'),
+        },
+        {
+          fullDescription: new RegExp('.*' + text + '.*', 'i'),
+        },
+        {
+          abbreviatedDescription: new RegExp('.*' + text + '.*', 'i'),
+        },
+        {
+          categoryTitle: new RegExp('.*' + text + '.*', 'i'),
+        },
+      ],
+    }).select('-__v');
+  },
 };
-
-async function getAll() {
-  return await Icd10Code.find().select('-__v');
-}
-
-async function getById(id) {
-  return await Icd10Code.findById(id).select('-__v');
-}
-
-async function getByFullCode(fullCode) {
-  return await Icd10Code.find({ fullCode: fullCode }).select('-__v');
-}
